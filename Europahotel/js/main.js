86bcf52c2e65e30a3b6f9b15ccbd6ad6db476487
JohnCletus001 <71078838+JohnCletus001@users.noreply.github.com>
2020-11-01 15:27:51 +0100

    Add files via upload

---
+++ Europahotel/js/main.js
@@ -0,0 +1,23 @@
+/*  ---------------------------------------------------
+    Template Name: Europa Hotel And Spa
+    Description: Europa Hotel And Spa HTML Template
+    Author: Colorlib
+    Author URI: http://www.colorlib.com
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $("#preloader, .loader-img, .spinner").delay(800).fadeOut("slow");
+    });
+    // /*------------------
+	// 	Date Picker
+	// --------------------*/
+})(jQuery);
